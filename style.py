d525da73f71fbc6448ecb882d59913514b79040b
MangriMen <mangrimen@gmail.com>
2020-05-20 19:05:30 +0700

    added styles for QTabWidget and dice in rolls

--- style.py
+++ style.py
@@ -48,6 +48,44 @@ def setupMain(self):
     ShadowLabel(self)
 
 
+def setupCharGen(self):
+    QtGui.QFontDatabase.addApplicationFont('fonts/BOOKMANIA-REGULAR.TTF')
+    QtGui.QFontDatabase.addApplicationFont('fonts/EBERRON_RUS.TTF')
+
+    self.setWindowFlags(QtCore.Qt.Widget | QtCore.Qt.FramelessWindowHint)
+    self.setAttribute(Qt.WA_TranslucentBackground)
+    self.setStyleSheet(getWindowStyleSheet())
+
+    self.ui.label.setStyleSheet(getLabelStyleSheet())
+
+    self.ui.btnClose.setStyleSheet(getCloseStyleSheet())
+    self.ui.btnClose.setIcon(QtGui.QIcon('images/buttons/close.png'))
+    self.ui.btnClose.setIconSize(QtCore.QSize(10, 10))
+
+    self.ui.btnMaximize.setStyleSheet(getMaximizeStyleSheet())
+    self.ui.btnMaximize.setIcon(QtGui.QIcon('images/buttons/maximize.png'))
+    self.ui.btnMaximize.setIconSize(QtCore.QSize(10, 10))
+
+    self.ui.btnMinimize.setStyleSheet(getMinimizeStyleSheet())
+    self.ui.btnMinimize.setIcon(QtGui.QIcon('images/buttons/minimize.png'))
+    self.ui.btnMinimize.setIconSize(QtCore.QSize(10, 10))
+
+    self.ui.toolBar.setStyleSheet(getToolBarStyleSheet())
+
+    self.ui.temperBox.setAttribute(Qt.WA_TranslucentBackground)
+
+    self.ui.classSkillsChoose.setStyleSheet(getChooseWidgetStyleSheet())
+    self.ui.backgroundSpecChoose.setStyleSheet(getChooseWidgetStyleSheet())
+
+    self.ui.raceAddLanguageCombo.hide()
+    self.ui.raceAddLanguageLabel.hide()
+
+    CreateWindowShadow(self)
+    ShadowLineEdit(self)
+    ShadowRadioButton(self)
+    ShadowLabel(self)
+
+
 def CreateLineEditShadow(self, lineEdit):
     shadowEffect = QtWidgets.QGraphicsDropShadowEffect(self)
     shadowEffect.setBlurRadius(5)
@@ -116,6 +154,21 @@ def getWindowStyleSheet():
             "QPushButton#menuButton {"
             "border: none"
             "}"
+            "QWidget#copper {"
+            "border-image: url('images/money/copper.png') 0 0 0 0 stretch stretch;"
+            "}"
+            "QWidget#silver {"
+            "border-image: url('images/money/silver.png') 0 0 0 0 stretch stretch;"
+            "}"
+            "QWidget#electrum {"
+            "border-image: url('images/money/electrum.png') 0 0 0 0 stretch stretch;"
+            "}"
+            "QWidget#gold {"
+            "border-image: url('images/money/gold.png') 0 0 0 0 stretch stretch;"
+            "}"
+            "QWidget#platinum {"
+            "border-image: url('images/money/platinum.png') 0 0 0 0 stretch stretch;"
+            "}"
             "QFrame#temperBox {"
             "background-color: #efe7cf;;"
             "border: 2px ridge #6e3320;"
@@ -136,6 +189,77 @@ def getWindowStyleSheet():
             "}"
             "QRadioButton::indicator:unchecked {"
             "image: url('images/buttons/radio_unchecked.png');"
+            "}"
+            "QComboBox {"
+            "background-color: #dcc48d;"
+            "border: 2px ridge #6e3320;"
+            "border-radius: 3px"
+            "}"
+            "QComboBox QAbstractItemView {"
+            "selection-background-color: #e5d297;"
+            "background-color: #dcc48d;"
+            "border: 2px ridge #6e3320;"
+            "border-radius: 3px"
+            "}"
+            "QComboBox QScrollBar:vertical {"
+            "selection-background-color: #e5d297;"
+            "background-color: #dcc48d;"
+            "}"
+            "QPushButton#btnDone {"
+            "border: none;"
+            "font: 28pt \"Eberron\";"
+            "color: #fff;"
+            "background-color: #681e22;"
+            "}"
+            "QPushButton#btnDone::disabled {"
+            "color: #bdbdbd;"
+            "background-color: #693e40;"
+            "}"
+            "QPushButton#btnDone::hover {"
+            "background-color: #752226;"
+            "}"
+            "QWidget#skillsChooseBox {"
+            "background-color: #dcc48d;"
+            "border: 2px ridge #6e3320;"
+            "border-radius: 3px;"
+            "}"
+            "QWidget#skillsChooseBox::disabled {"
+            "background-color: #cbbe98;"
+            "border: 2px ridge #6e3320;"
+            "border-radius: 3px;"
+            "}"
+            "QWidget::tab-bar {"
+            "left: 3px;"
+            "}"
+            "QTabWidget::pane {"
+            "background-color: #dcc48d;"
+            "border: 3px solid #681e22;"
+            "border-radius: 3px;"
+            "top: -2px;"
+            "}"
+            "QTabBar::tab {"
+            "color: white;"
+            "background-color: #82262a;"
+            "border: 2px solid #4f171a;"
+            "border-bottom-color:: #dcc48d;"
+            "border-top-left-radius: 4px;"
+            "border-top-right-radius: 4px;"
+            "padding: 2px;"
+            "min-width: 8ex;"
+            "}"
+            "QTabBar::tab:selected, QTabBar::tab:hover {"
+            "background-color: #9c2d33;"
+            "}"
+            "QTabBar::tab:selected {"
+            "border-bottom-color:: #dcc48d;"
+            "}"
+            "QFrame#raceBox, QFrame#alignmentBox, QFrame#classBox, QFrame#backgroundBox {"
+            "border: 3px solid #6e3320;"
+            "border-radius: 4px;"
+            "}"
+            "QPushButton#personalityRoll, QPushButton#idealsRoll, QPushButton#bondsRoll, QPushButton#flawsRoll {"
+            "background: #00ffffff;"
+            "image: url('images/buttons/D6_0.png');"
             "}")
 
 
@@ -234,4 +358,26 @@ def getToolBarStyleSheet():
     return ("QWidget {"
             "background-color: #681e22;"
             "border: none;"
+            "}")
+
+
+def getChooseWidgetStyleSheet():
+    return ("QWidget {"
+            "background-color: #dcc48d;"
+            "border: 2px ridge #6e3320;"
+            "border-radius: 3px;"
+            "}"
+            )
+
+
+def getChooseWidgetInsideStyleSheet():
+    return ("QWidget {"
+            "border: none"
+            "}")
+
+
+def rollD6(roll):
+    return ("QPushButton {"
+            "background: #00ffffff;"
+            "image: url('images/buttons/D6_" + str(roll + 1) + ".png');"
             "}")
