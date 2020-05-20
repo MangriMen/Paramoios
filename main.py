c643678423ef1ace5f4e20968e2764f77b22c85b
MangriMen <mangrimen@gmail.com>
2020-05-20 17:27:00 +0700

    deleted unused QIcon import

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from PyQt5.QtGui import QIcon
 from MainWindow import MainWindow
 
 if __name__ == '__main__':
-	
+
     app = QApplication(sys.argv)
 
     w = MainWindow()
