256a8c677d57ba951ae223f3b3c4d1331f6d03fa
FFreestyler <freeestyler231@gmail.com>
2020-07-22 19:42:35 +0600

    Added function for button 'heal' and 'damage'

---
+++ javascript/onclick.js
@@ -0,0 +1,28 @@
+"use strict"
+
+function healhp(){
+	let hp=document.getElementById("hp").textContent;
+	let newhp=0;
+	let final="";
+	hp=hp.split("/");
+	if(Number(hp[0])<Number(hp[1])){
+		newhp=Number(hp[0])+1;
+	}
+	else {
+		newhp=Number(hp[0]);
+	}
+	final=newhp+"/"+hp[1];
+	document.getElementById("hp").textContent=final;
+}
+
+function damagehp(){
+	let hp=document.getElementById("hp").textContent;
+	let newhp=0;
+	let final="";
+	hp=hp.split("/");
+	if(Number(hp[0])>0){
+		newhp=Number(hp[0])-1;
+	}
+	final=newhp+"/"+hp[1];
+	document.getElementById("hp").textContent=final;
+}
