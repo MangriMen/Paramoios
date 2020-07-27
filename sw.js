4358d926457684cbdef97e7e4731024775287153
MangriMen <mangrimen@gmail.com>
2020-07-27 18:56:17 +0600

    added holder page and fixed pwa

---
+++ sw.js
@@ -0,0 +1,20 @@
+// install event
+self.addEventListener('install', evt => {
+  //console.log('service worker installed');
+  evt.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log('caching shell assets');
+      cache.addAll(assets);
+    })
+  );
+});
+
+// activate event
+self.addEventListener('activate', evt => {
+  //console.log('service worker activated');
+});
+
+// fetch event
+self.addEventListener('fetch', evt => {
+  //console.log('fetch event', evt);
+});
