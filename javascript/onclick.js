004e5ea19763837cffaae02834ec81d7f84008c8
MangriMen <mangrimen@gmail.com>
2020-07-28 20:48:21 +0600

    Added animation and depending on actual hp to hp bar

--- javascript/onclick.js
+++ javascript/onclick.js
@@ -1,18 +1,33 @@
 "use strict"
 
 function changeHp(isHealed) {
-	let oldHp = (document.getElementById("hp").textContent).split("/");
-	let actualHp = Number(oldHp[0]);
-	let maxHp = Number(oldHp[1]);
-    let newHp = 0;
-    let final = "";
+  let oldHp = (document.getElementById("hp").textContent).split("/");
+  let actualHp = Number(oldHp[0]);
+  let maxHp = Number(oldHp[1]);
+  let newHp = 0;
+  let final = "";
 
-    if (isHealed) {
-		newHp = actualHp + (actualHp < maxHp);
-    } else {
-		newHp = actualHp - (actualHp > 0);
-    }
-    
-    final = newHp + "/" + maxHp;
-    document.getElementById("hp").textContent = final;
+  if (isHealed) {
+  newHp = actualHp + (actualHp < maxHp);
+  } else {
+  newHp = actualHp - (actualHp > 0);
+  }
+
+  final = newHp + "/" + maxHp;
+  document.getElementById("hp").textContent = final;
+
+  changeWidth(newHp, maxHp);
+}
+
+function onloadWidth() {
+  let oldHp = (document.getElementById("hp").textContent).split("/");
+  let actualHp = Number(oldHp[0]);
+  let maxHp = Number(oldHp[1]);
+  changeWidth(actualHp, maxHp);
+}
+
+function changeWidth(newHp, maxHp) {
+  let maxWidth = document.getElementById('health-bar').offsetWidth;
+  let newWidth = (newHp / maxHp) * maxWidth;
+  document.getElementById('health-bar-fill').style.width = newWidth + "px";
 }
