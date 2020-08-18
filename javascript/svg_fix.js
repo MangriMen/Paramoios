5016f580cda8d17c703cd404b951c208a40ef1d0
MangriMen <mangrimen@gmail.com>
2020-08-19 01:10:58 +0600

    Rewrited svg-fix to pure js, added beautiful alert

--- javascript/svg_fix.js
+++ javascript/svg_fix.js
@@ -1,18 +1,40 @@
 "use strict"
 
-$('img.img-svg').each(function(){
-  var $img = $(this);
-  var imgClass = $img.attr('class');
-  var imgURL = $img.attr('src');
-  $.get(imgURL, function(data) {
-    var $svg = $(data).find('svg');
-    if(typeof imgClass !== 'undefined') {
-      $svg = $svg.attr('class', imgClass+' replaced-svg');
+function htmlToElement(html) {
+  var template = document.createElement('template');
+  html = html.trim();
+  template.innerHTML = html;
+  return template.content.firstChild;
+}
+
+var HttpClient = function () {
+  this.get = function (aUrl, aCallback) {
+    var anHttpRequest = new XMLHttpRequest();
+    anHttpRequest.onreadystatechange = function () {
+      if (anHttpRequest.readyState == 4 && anHttpRequest.status == 200)
+        aCallback(anHttpRequest.response);
     }
-    $svg = $svg.removeAttr('xmlns:a');
-    if(!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
-      $svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'))
+    anHttpRequest.open("GET", aUrl, true);
+    anHttpRequest.send(null);
+  }
+}
+
+var client = new HttpClient();
+
+let svgArr = Array.from(document.getElementsByClassName('img-svg'));
+
+for (let svgItem in svgArr) {
+  let img = svgArr[svgItem];
+  let imgId = img.id;
+  let imgClass = img.className;
+  let imgURL = img.src;
+  console.log(img);
+
+  client.get(imgURL, function(data) {
+    let svg = htmlToElement(data);
+    img.parentElement.replaceChild(svg, img);
+    if (typeof imgClass !== 'undefined') {
+      svg.classList = imgClass + ' replaced-svg';
     }
-    $img.replaceWith($svg);
-  }, 'xml');
-});
+  })
+}
