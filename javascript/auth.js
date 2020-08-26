abaac56e43b285eb09cbe01ad4ef014de55147e7
MangriMen <mangrimen@gmail.com>
2020-08-26 14:08:10 +0600

    Added form auth and registration, user avatar loading

---
+++ javascript/auth.js
@@ -0,0 +1,14 @@
+'use strict'
+
+$(document).ready(function () {
+    const request = fetch('../avatar.php')
+
+    const jsonStream = request.then(text => { return text.text() });
+
+    jsonStream.then(data => {
+        let img = document.createElement('img');
+        img.src = "data:image/jpeg;base64," + data;
+        img.id = 'user-btn-img';
+        document.getElementById('user-btn').appendChild(img);
+    });
+})
