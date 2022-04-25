fcf00d50ed18d35f0f1566817afc76bbb7699230
Menchshikov Danil <freeestyler231@gmail.com>
2022-04-26 00:53:54 +0700

    Started the creation user card

--- src/components/user/UserCardComponent.tsx
+++ src/components/user/UserCardComponent.tsx
@@ -1,21 +1,80 @@
-import { Box, Typography } from "@mui/material";
-import { getAuth } from "firebase/auth";
-import "helpers/firebase";
+import { Avatar, Box, Typography, useTheme } from "@mui/material";
+
+function stringToColor(string: string) {
+  let hash = 0;
+  let i;
+
+  /* eslint-disable no-bitwise */
+  for (i = 0; i < string.length; i += 1) {
+    hash = string.charCodeAt(i) + ((hash << 5) - hash);
+  }
+
+  let color = "#";
+
+  for (i = 0; i < 3; i += 1) {
+    const value = (hash >> (i * 8)) & 0xff;
+    color += `00${value.toString(16)}`.slice(-2);
+  }
+  /* eslint-enable no-bitwise */
+
+  return color;
+}
+
+function stringAvatar(name: string) {
+  return {
+    sx: {
+      bgcolor: stringToColor(name),
+    },
+    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
+  };
+}
 
 function UserCardComponent() {
-  //   const user = useSelector(getUser);
+  //   const user = useSelector(getUser)
 
-  const auth = getAuth();
-  const user = auth?.currentUser;
+  const theme = useTheme();
 
   return (
     <>
-      {user && (
-        <Box sx={{ display: "flex", flexDirection: "column" }}>
-          <Typography variant="h1">Name: {user.displayName}</Typography>
-          <Typography variant="h1">Email: {user.email}</Typography>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "center",
+          mt: "1rem",
+          mb: "1rem",
+          flexWrap: "wrap",
+        }}
+      >
+        <Avatar
+          {...stringAvatar("Kent Dodds")}
+          variant="square"
+          sx={{
+            width: "200px",
+            height: "200px",
+            border: "4px solid",
+            borderRadius: "4px",
+            borderColor: theme.palette.primary.main,
+            fontSize: "64px",
+          }}
+        ></Avatar>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            ml: "1rem",
+            fontSize: "32px",
+            flexWrap: "wrap",
+          }}
+        >
+          <Typography variant="h3" textAlign="center" fontSize="inherit">
+            Name: LoremIpsumLoremIpsum
+          </Typography>
+          <Typography variant="h3" textAlign="center" fontSize="inherit">
+            Email: LoremIpsumLoremIpsum
+          </Typography>
         </Box>
-      )}
+      </Box>
     </>
   );
 }
--- src/components/user/UserPageComponent.tsx
+++ src/components/user/UserPageComponent.tsx
@@ -1,11 +1,22 @@
-import { Box } from "@mui/material";
+import { Container, useTheme } from "@mui/material";
 import UserCardComponent from "./UserCardComponent";
 
 function UserPageComponent() {
+  const theme = useTheme();
+
   return (
-    <Box maxWidth="lg" display="flex" justifyContent="center">
+    <Container
+      maxWidth="lg"
+      sx={{
+        mt: "1rem",
+        border: "4px solid",
+        borderColor: theme.palette.primary.main,
+        borderRadius: "4px",
+        backgroundColor: theme.palette.secondary.main,
+      }}
+    >
       <UserCardComponent />
-    </Box>
+    </Container>
   );
 }
 
