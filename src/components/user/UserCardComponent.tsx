42c660c074ab95772bae76c21327809c317c0aeb
Menchshikov Danil <freeestyler231@gmail.com>
2022-04-27 16:26:18 +0700

    Minor Avatar fixes

--- src/components/user/UserCardComponent.tsx
+++ src/components/user/UserCardComponent.tsx
@@ -1,36 +1,9 @@
 import { Avatar, Box, Typography, useTheme } from "@mui/material";
-
-function stringToColor(string: string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = "#";
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
-function stringAvatar(name: string) {
-  return {
-    sx: {
-      bgcolor: stringToColor(name),
-    },
-    children: `${name.split(" ")[0][0]}${name.split(" ")[1][0]}`,
-  };
-}
+import { getAuth } from "firebase/auth";
 
 function UserCardComponent() {
-  //   const user = useSelector(getUser)
+  const auth = getAuth();
+  const user = auth?.currentUser;
 
   const theme = useTheme();
 
@@ -47,7 +20,6 @@ function UserCardComponent() {
         }}
       >
         <Avatar
-          {...stringAvatar("Kent Dodds")}
           variant="square"
           sx={{
             width: "200px",
@@ -55,23 +27,29 @@ function UserCardComponent() {
             border: "4px solid",
             borderRadius: "4px",
             borderColor: theme.palette.primary.main,
-            fontSize: "64px",
+            fontSize: "128px",
           }}
-        ></Avatar>
+        >
+          {user?.displayName?.toString().charAt(0)}
+        </Avatar>
         <Box
           sx={{
             display: "flex",
             flexDirection: "column",
             ml: "1rem",
-            fontSize: "32px",
+            mr: "1rem",
+            fontSize: {
+              lg: "32px",
+              xs: "28px",
+            },
             flexWrap: "wrap",
           }}
         >
-          <Typography variant="h3" textAlign="center" fontSize="inherit">
-            Name: LoremIpsumLoremIpsum
+          <Typography variant="h3" fontSize="inherit">
+            Name: {user?.displayName}
           </Typography>
-          <Typography variant="h3" textAlign="center" fontSize="inherit">
-            Email: LoremIpsumLoremIpsum
+          <Typography variant="h3" fontSize="inherit">
+            Email: {user?.email}
           </Typography>
         </Box>
       </Box>
