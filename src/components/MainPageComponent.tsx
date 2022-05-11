69b2aedbaa81658e25e5ae3aebbaa99d3e83f0d8
FFreestyler <freeestyler231@gmail.com>
2022-05-11 23:23:08 +0700

    Create feature/#6 branch and started grid

--- src/components/MainPageComponent.tsx
+++ src/components/MainPageComponent.tsx
@@ -1,4 +1,4 @@
-import { Container, Typography, useTheme } from '@mui/material';
+import { Box, Container, Grid, Typography, useTheme } from '@mui/material';
 
 function MainPageComponent() {
   const theme = useTheme();
@@ -11,10 +11,38 @@ function MainPageComponent() {
         border: '4px solid',
         borderColor: theme.palette.primary.main,
         borderRadius: '4px',
-        height: '500px',
+        padding: '1rem',
+        flexGrow: '1',
       }}
     >
-      <Typography>Hello World</Typography>
+      <Grid container spacing={4}>
+        <Grid item xs={2}>
+          <Box
+            sx={{
+              border: '4px solid',
+              borderColor: theme.palette.primary.main,
+              backgroundColor: theme.palette.secondary.main,
+              borderRadius: '4px',
+              height: '50%',
+              padding: '2rem',
+            }}
+          ></Box>
+        </Grid>
+        <Grid item xs={10}>
+          <Typography sx={{ fontSize: '5rem' }}>Hello World</Typography>
+        </Grid>
+        <Grid item xs={2}>
+          <Box
+            sx={{
+              border: '4px solid',
+              borderColor: theme.palette.primary.main,
+              backgroundColor: theme.palette.secondary.main,
+              borderRadius: '4px',
+              padding: '2rem',
+            }}
+          ></Box>
+        </Grid>
+      </Grid>
     </Container>
   );
 }
