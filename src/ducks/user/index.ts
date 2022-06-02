6ffbaa3e6b70f4a44b000e5b0e2949b88f0268e4
RomaS <stepanovroma2019@gmail.com>
2022-06-02 15:45:12 +0700

    Added slice to update user

--- src/ducks/user/index.ts
+++ src/ducks/user/index.ts
@@ -1,17 +1,43 @@
 import { createSlice } from '@reduxjs/toolkit';
 
-import { getUser } from './selectors';
-
-const initialState = { user: { name: '', email: '' } };
+const initialState = {
+  error: null,
+  loading: false,
+  userInfo: {
+    userName: '',
+    email: '',
+    password: '',
+  },
+  type: 0,
+};
 
 export const userSlice = createSlice({
   name: '@@user',
   initialState,
   reducers: {
-    updateUser: (state, action) => {
-      state.user = action.payload;
+    updateUser(state, action) {
+      state.error = null;
+      state.loading = true;
+      state.userInfo = {
+        userName: '',
+        email: '',
+        password: '',
+      };
+      state.type = 0;
+    },
+    updateUserSuccess(state, action) {
+      state.error = null;
+      state.loading = false;
+      state.userInfo = action.payload;
+      state.type = action.payload;
+    },
+    updateUserError(state, action) {
+      state.error = action.payload;
+      state.loading = false;
     },
   },
 });
 
-export { getUser };
+export default userSlice.reducer;
+export const { updateUser, updateUserSuccess, updateUserError } =
+  userSlice.actions;
