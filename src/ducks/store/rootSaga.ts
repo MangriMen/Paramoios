0728d485c01fab18b229181690f667a763a65551
RomaS <stepanovroma2019@gmail.com>
2022-06-02 15:44:35 +0700

    Added update sagawatcher in rootsaga

--- src/ducks/store/rootSaga.ts
+++ src/ducks/store/rootSaga.ts
@@ -1,6 +1,7 @@
 import { watchAuth } from 'ducks/auth/sagas';
+import { updateSagaWatcher } from 'ducks/user/sagas';
 import { all, call } from 'redux-saga/effects';
 
 export function* rootSaga() {
-  yield all([call(watchAuth)]);
+  yield all([call(watchAuth), call(updateSagaWatcher)]);
 }
