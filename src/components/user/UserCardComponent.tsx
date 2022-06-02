6d0dcb4190cb6fbe4a0db32e1ffcfead3a574897
RomaS <stepanovroma2019@gmail.com>
2022-06-02 17:26:37 +0700

    Changed design

--- src/components/user/UserCardComponent.tsx
+++ src/components/user/UserCardComponent.tsx
@@ -1,88 +1,110 @@
-import { Avatar, Box, Button, Typography, useTheme } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+import ParAvatar from 'components/styled/ParAvatar';
+import ParLink from 'components/styled/ParLink';
 import { ROUTE } from 'consts';
 import { auth } from 'helpers/firebase';
 import { useNavigate } from 'react-router-dom';
 
-function stringToColor(string: string) {
-  let hash = 0;
-  let i;
-
-  /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
-    hash = string.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  let color = '#';
-
-  for (i = 0; i < 3; i += 1) {
-    const value = (hash >> (i * 8)) & 0xff;
-    color += `00${value.toString(16)}`.slice(-2);
-  }
-  /* eslint-enable no-bitwise */
-
-  return color;
-}
-
-function stringAvatar(name: string) {
-  return `${name.charAt(0)}`;
-}
-
 function UserCardComponent() {
   const user = auth?.currentUser;
-  const theme = useTheme();
   const navigate = useNavigate();
 
   return (
-    <>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+      }}
+    >
+      <ParAvatar
+        variant="square"
+        sx={{
+          width: '15rem',
+          height: '15rem',
+        }}
+      >
+        {user?.displayName}
+      </ParAvatar>
+      <Box
+        sx={{
+          width: {
+            xs: '100%',
+            md: 'auto',
+          },
+          display: 'flex',
+          flexDirection: 'column',
+          fontSize: {
+            lg: '32px',
+            xs: '28px',
+          },
+          flexWrap: 'wrap',
+          padding: '1rem',
+        }}
+      >
+        <Typography
+          variant="h3"
+          fontSize="inherit"
+          sx={{
+            wordWrap: 'break-word',
+            width: '100%',
+          }}
+          color="primary"
+        >
+          Name:
+        </Typography>
+        <Typography
+          variant="h3"
+          fontSize="inherit"
+          sx={{
+            wordWrap: 'break-word',
+            width: '100%',
+          }}
+        >
+          {user?.displayName}
+        </Typography>
+        <Typography
+          variant="h3"
+          fontSize="inherit"
+          sx={{
+            wordWrap: 'break-word',
+            width: '100%',
+          }}
+          color="primary"
+        >
+          Email:
+        </Typography>
+        <Typography
+          variant="h3"
+          fontSize="inherit"
+          sx={{
+            wordWrap: 'break-word',
+            width: '100%',
+          }}
+        >
+          {user?.email}
+        </Typography>
+      </Box>
+
       <Box
         sx={{
           display: 'flex',
-          flexDirection: 'row',
-          justifyContent: 'space-between',
-          mt: '1rem',
-          mb: '1rem',
-          flexWrap: 'wrap',
+          flexDirection: 'column',
+          alignItems: 'flex-end',
+          flexGrow: '1',
         }}
       >
-        <Avatar
-          variant="square"
-          sx={{
-            backgroundColor: stringToColor(String(user?.displayName)),
-            width: '200px',
-            height: '200px',
-            border: '4px solid',
-            borderRadius: '4px',
-            borderColor: theme.palette.primary.main,
-            fontSize: '128px',
-          }}
-          children={stringAvatar(String(user?.displayName))}
-        />
-        <Box
-          sx={{
-            display: 'flex',
-            flexDirection: 'column',
-            ml: '1rem',
-            mr: '1rem',
-            fontSize: {
-              lg: '32px',
-              xs: '28px',
-            },
-            flexWrap: 'wrap',
-            padding: '2vh',
-          }}
+        <ParLink
+          color="primary"
+          component="button"
+          onClick={() => navigate(ROUTE.SETTINGS)}
+          sx={{ textShadow: '1px 1px 1px #681e22' }}
         >
-          <Typography variant="h3" fontSize="inherit">
-            Name: {user?.displayName}
-          </Typography>
-          <Typography variant="h3" fontSize="inherit">
-            Email: {user?.email}
-          </Typography>
-        </Box>
-        <Box>
-          <Button onClick={() => navigate(ROUTE.SETTINGS)}>Settings</Button>
-        </Box>
+          Settings
+        </ParLink>
       </Box>
-    </>
+    </Box>
   );
 }
 
