eb2e2314b2c463bd231e56b76f2b3f72096d1ac6
RomaS <stepanovroma2019@gmail.com>
2022-06-03 14:03:28 +0700

    Added userInfo data

---
+++ src/mocks/mockUserInfo.tsx
@@ -0,0 +1,23 @@
+export const userInfo = {
+  userName: 'Robot',
+  email: 'Robor@gmail.com',
+  otherInfo: 'I was born in 1939',
+  descriptions:
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum',
+  userCharacters: [
+    {
+      idLink: '1',
+      lvl: '16',
+      race: 'Black stone',
+      class: 'Rouge',
+      img: 'https://static.mineralmarket.ru/img/p/9633-89373.jpg',
+    },
+    {
+      idLink: '2',
+      lvl: '1',
+      race: 'White stone',
+      class: 'Source',
+      img: 'https://www.stroyportal.ru/media/cache/companies/176956/products/690400164/16599397_image_large.jpg',
+    },
+  ],
+};
