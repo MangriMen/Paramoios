a50068414f4a64dcdaff1e6cec6a97d490ce8238
MangriMen <mangrimen@gmail.com>
2022-06-06 00:19:33 +0700

    Written jsdoc for helpers

--- src/helpers/avatar.ts
+++ src/helpers/avatar.ts
@@ -1,17 +1,21 @@
+/**
+ * Used to get color as a hash of the input string for automatically generated avatars
+ * @param string any string, can be user name/nickname
+ * @returns color in hex with alpha, calculated as a hash of the input string
+ */
 export function stringToColor(string: string | null | undefined) {
   if (string === undefined || string === null) return undefined;
 
   let hash = 0;
-  let i;
 
   /* eslint-disable no-bitwise */
-  for (i = 0; i < string.length; i += 1) {
+  for (let i = 0; i < string.length; i += 1) {
     hash = string.charCodeAt(i) + ((hash << 5) - hash);
   }
 
   let color = '#';
 
-  for (i = 0; i < 3; i += 1) {
+  for (let i = 0; i < 3; i += 1) {
     const value = (hash >> (i * 8)) & 0xff;
     color += `00${value.toString(16)}`.slice(-2);
   }
@@ -20,6 +24,11 @@ export function stringToColor(string: string | null | undefined) {
   return color;
 }
 
+/**
+ * Used to get  first letter of the input string for automatically generated avatars
+ * @param name - real name/nikname
+ * @returns first letter of name
+ */
 export function stringAvatar(name: string | null | undefined) {
   return name === undefined || name === null ? undefined : `${name.charAt(0)}`;
 }
--- src/helpers/common.ts
+++ src/helpers/common.ts
@@ -1,9 +1,15 @@
+/**
+ * Used to call a function with exception handling and get the result in place
+ * @param function_ - callable function
+ * @param args - arguments for the callable function
+ * @returns the result of a function call, or undefined when an exception occurs
+ */
 export function tryCall<T extends (...args: Parameters<T>) => ReturnType<T>>(
-  tryFunction: T,
+  function_: T,
   ...args: Parameters<T>
 ): ReturnType<T> | undefined {
   try {
-    return tryFunction(...args);
+    return function_(...args);
   } catch (e) {
     if (e instanceof Error) {
       console.log(e.message);
