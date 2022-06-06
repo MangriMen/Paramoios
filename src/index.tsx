e36a5021988d11e86b4624205bffd3baddc78b0a
MangriMen <mangrimen@gmail.com>
2022-06-06 23:18:59 +0700

    Fixed broken imports

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import 'assets/styles/index.css';
 import App from 'components/App';
+import 'configs/firebase.ts';
 import 'configs/i18next';
 import { store } from 'ducks/store';
-import 'helpers/firebase.ts';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
