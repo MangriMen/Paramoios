bfa40cfdd7ce474a3b15870e4200be67f424a218
MangriMen <mangrimen@gmail.com>
2022-06-09 20:30:08 +0700

    Added getIsLoading selector

--- src/ducks/auth/selectors.ts
+++ src/ducks/auth/selectors.ts
@@ -1,5 +1,9 @@
 import { RootState } from 'ducks/store';
 
+export function getIsLoading(state: RootState) {
+  return state.auth.isLoading;
+}
+
 export function getIsLogged(state: RootState) {
   return state.auth.isLogged;
 }
