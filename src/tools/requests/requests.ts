3b9f5ccfed9ddaaa82627276982e525ecdc9d4db
RomaS <stepanovroma2019@gmail.com>
2022-06-10 23:06:38 +0700

    Added requests to avatar

--- src/tools/requests/requests.ts
+++ src/tools/requests/requests.ts
@@ -1,5 +1,6 @@
-import { auth } from 'configs/firebase';
+import { auth, storage } from 'configs/firebase';
 import { updateEmail, updatePassword, updateProfile } from 'firebase/auth';
+import { getDownloadURL, ref, uploadString } from 'firebase/storage';
 
 export function setUserDisplayName(payload: any) {
   if (auth?.currentUser) {
@@ -16,5 +17,24 @@ export function setUserEmail(payload: any) {
 export function setUserPassword(payload: any) {
   if (auth?.currentUser) {
     return updatePassword(auth.currentUser, `${payload.password}`);
+  }
+}
+
+export function setUserAvatar(payload: any) {
+  if (auth?.currentUser) {
+    return updateProfile(auth.currentUser, { photoURL: payload });
+  }
+}
+
+export function uploadFileToStorage(payload: any) {
+  if (auth?.currentUser && storage) {
+    const storageRef = ref(storage, auth.currentUser.uid);
+    return uploadString(storageRef, payload, 'data_url');
+  }
+}
+
+export function getFileUrlFromStorage(payload: any) {
+  if (auth?.currentUser) {
+    return getDownloadURL(payload);
   }
 }
