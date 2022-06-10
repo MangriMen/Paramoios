a6c80b20996981621eb70df0ba86946d3720ce62
RomaS <stepanovroma2019@gmail.com>
2022-06-10 23:04:05 +0700

    Added new user reducer and saga

--- src/ducks/store/rootReducer.ts
+++ src/ducks/store/rootReducer.ts
@@ -1,7 +1,12 @@
 import { authSlice } from 'ducks/auth';
 import userReducer from 'ducks/user';
+import userSettingsReducer from 'ducks/userSettings';
 import { combineReducers } from 'redux';
 
 export const rootReducer = () => {
-  return combineReducers({ auth: authSlice.reducer, user: userReducer });
+  return combineReducers({
+    auth: authSlice.reducer,
+    user: userReducer,
+    userSettings: userSettingsReducer,
+  });
 };
--- src/ducks/store/rootSaga.ts
+++ src/ducks/store/rootSaga.ts
@@ -1,7 +1,8 @@
 import { watchAuth } from 'ducks/auth/sagas';
-import { updateSagaWatcher } from 'ducks/user/sagas';
+import { fetchSagaWatcher } from 'ducks/user/sagas';
+import { updateSagaWatcher } from 'ducks/userSettings/sagas';
 import { all, call } from 'redux-saga/effects';
 
 export function* rootSaga() {
-  yield all([call(watchAuth), call(updateSagaWatcher)]);
+  yield all([call(watchAuth), call(fetchSagaWatcher), call(updateSagaWatcher)]);
 }
