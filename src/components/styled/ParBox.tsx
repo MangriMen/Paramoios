46d0084c9f74476cf8210eb5a20b0981d15b7fea
MangriMen <mangrimen@gmail.com>
2022-06-24 06:29:39 +0700

    Implemented title on ParBox

--- src/components/styled/ParBox.tsx
+++ src/components/styled/ParBox.tsx
@@ -1,19 +1,48 @@
-import { Box, BoxProps, styled } from '@mui/material';
+import { Box, BoxProps, Typography } from '@mui/material';
 import { FC } from 'react';
 
-const BoxStyled = styled(Box)(({ theme }) => ({}));
+interface ParBoxProps extends BoxProps {
+  title?: string;
+}
 
-const ParBox: FC<BoxProps> = ({ children, ...props }) => {
+const TITLE_HEIGHT = '0.9rem';
+
+const ParBox: FC<ParBoxProps> = ({ title, children, ...props }) => {
   return (
-    <BoxStyled
-      bgcolor="secondary.main"
-      border="4px solid"
-      borderColor="primary.main"
-      borderRadius="4px"
-      {...props}
+    <Box
+      position="relative"
+      display="flex"
+      flexDirection="column"
+      alignItems="center"
+      marginTop={TITLE_HEIGHT}
     >
-      {children}
-    </BoxStyled>
+      {title && (
+        <Typography
+          boxSizing="border-box"
+          position="absolute"
+          top={`-${TITLE_HEIGHT}`}
+          minWidth="20%"
+          padding="0 0.4rem"
+          textAlign="center"
+          bgcolor="secondary.main"
+          border="4px solid"
+          borderColor="primary.main"
+          borderRadius="4px"
+        >
+          {title}
+        </Typography>
+      )}
+      <Box
+        alignSelf="stretch"
+        bgcolor="secondary.main"
+        border="4px solid"
+        borderColor="primary.main"
+        borderRadius="4px"
+        {...props}
+      >
+        {children}
+      </Box>
+    </Box>
   );
 };
 
