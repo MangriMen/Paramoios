0f7e91b28a16f31e246c7a879da6b53815385ab8
MangriMen <mangrimen@gmail.com>
2022-06-24 18:09:55 +0700

    Fixed requests, now throw exception when fail

--- src/tools/requests/requests.ts
+++ src/tools/requests/requests.ts
@@ -8,38 +8,53 @@ import {
 } from 'firebase/storage';
 
 export function setUserDisplayName(payload: string) {
-  if (auth?.currentUser) {
-    return updateProfile(auth.currentUser, { displayName: payload });
+  if (auth?.currentUser === null || auth?.currentUser === undefined) {
+    throw new Error('Set user name failed. With currentUser being undefined');
   }
+  return updateProfile(auth.currentUser, { displayName: payload });
 }
 
 export function setUserEmail(payload: string) {
-  if (auth?.currentUser) {
-    return updateEmail(auth.currentUser, payload);
+  if (auth?.currentUser === null || auth?.currentUser === undefined) {
+    throw new Error('Set user email failed. With currentUser being undefined');
   }
+
+  return updateEmail(auth.currentUser, payload);
 }
 
 export function setUserPassword(payload: string) {
-  if (auth?.currentUser) {
-    return updatePassword(auth.currentUser, payload);
+  if (auth?.currentUser === null || auth?.currentUser === undefined) {
+    throw new Error(
+      'Set user password failed. With currentUser being undefined',
+    );
   }
+
+  return updatePassword(auth.currentUser, payload);
 }
 
 export function setUserAvatar(payload: string) {
-  if (auth?.currentUser) {
-    return updateProfile(auth.currentUser, { photoURL: payload });
+  if (auth?.currentUser === null || auth?.currentUser === undefined) {
+    throw new Error('Set user avatar failed. With currentUser being undefined');
   }
+
+  return updateProfile(auth.currentUser, { photoURL: payload });
 }
 
 export function uploadFileToStorage(payload: string) {
-  if (auth?.currentUser && storage) {
-    const storageRef = ref(storage, auth.currentUser.uid);
-    return uploadString(storageRef, payload, 'data_url');
+  if (auth?.currentUser === null || auth?.currentUser === undefined) {
+    throw new Error(
+      'Upload to storage failed. With currentUser being undefined',
+    );
   }
+
+  if (storage === undefined) {
+    throw new Error('Upload to storage failed. With storage being undefined');
+  }
+
+  const storageRef = ref(storage, auth.currentUser.uid);
+  return uploadString(storageRef, payload, 'data_url');
 }
 
 export function getFileUrlFromStorage(payload: StorageReference) {
-  if (auth?.currentUser) {
-    return getDownloadURL(payload);
-  }
+  return getDownloadURL(payload);
 }
