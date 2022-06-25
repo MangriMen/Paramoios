6eabe04c4ad2e2239b2700d8bba49d431c78b567
MangriMen <mangrimen@gmail.com>
2022-06-25 09:11:17 +0700

    Added multi-backend dependency

--- src/index.tsx
+++ src/index.tsx
@@ -5,6 +5,8 @@ import 'configs/firebase.ts';
 import 'configs/i18next';
 import { store } from 'ducks/store';
 import React from 'react';
+import { DndProvider } from 'react-dnd-multi-backend';
+import HTML5toTouch from 'react-dnd-multi-backend/dist/esm/HTML5toTouch';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
@@ -17,9 +19,11 @@ root.render(
   <React.StrictMode>
     <Provider store={store}>
       <ThemeProvider theme={defaultTheme}>
-        <BrowserRouter>
-          <App />
-        </BrowserRouter>
+        <DndProvider options={HTML5toTouch}>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </DndProvider>
       </ThemeProvider>
     </Provider>
   </React.StrictMode>,
