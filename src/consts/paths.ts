e138b718dc5e9ead3225f496998881a1f52eee26
MangriMen <mangrimen@gmail.com>
2022-07-04 15:44:39 +0700

    Returns absolute paths to consts/paths ROUTE

--- src/consts/paths.ts
+++ src/consts/paths.ts
@@ -1,8 +1,7 @@
 export const ROUTE = {
-  ROOT: '/',
-  HOME: '',
-  AUTH: 'auth',
-  ME: 'me',
-  SETTINGS: 'settings',
+  HOME: '/',
+  AUTH: '/auth',
+  ME: '/me',
+  SETTINGS: '/settings',
   PAGE_404: '*',
 };
