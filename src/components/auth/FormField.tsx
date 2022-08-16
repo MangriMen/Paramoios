9b1513bb3f7fd17bdd642004473731c60e579694
rstepanov <Roman.Stepanov@noveogroup.com>
2022-08-16 09:13:20 +0700

    Added merge in theme

--- src/components/auth/FormField.tsx
+++ src/components/auth/FormField.tsx
@@ -67,7 +67,6 @@ const AuthFormField: FC<AuthFormFieldProps> = ({
           color: `${calculatedColor}.main`,
         },
       }}
-      inputProps={{ maxLength: 90 }}
       {...props}
     >
       {children}
